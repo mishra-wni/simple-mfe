f7dc8ff60e7ae46c37eb6c1d8d60fd5aa2c3400e
wolfgang2612 <frozenthrone.ashutosh.mishra@gmail.com>
2021-09-10 13:04:06 +0530

    Added error boundary to host

--- host/src/App.js
+++ host/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./App.css";
 
+import ErrorBoundary from "./ErrorBoundary";
+
 const Remote1_A = React.lazy(() => import("Remote1/ComponentA"));
 
 function App() {
   return (
     <div>
       <h1>Host</h1>
-      <div className="remote-container">
-        <React.Suspense fallback="Loading Remote 1">
-          <Remote1_A />
-        </React.Suspense>
-      </div>
+      <ErrorBoundary>
+        <div className="remote-container">
+          <React.Suspense fallback="Loading Remote 1">
+            <Remote1_A />
+          </React.Suspense>
+        </div>
+      </ErrorBoundary>
     </div>
   );
 }
